986a3202dd627c93b7e6f52b7eac3e28aa42cf11
RakeshAmrutkar <41476036+RakeshAmrutkar@users.noreply.github.com>
2019-06-29 12:19:57 +0530

    updated

---
+++ app/src/main/java/com/example/android/kaunbanegacrorepati/Quizcontract.java
@@ -0,0 +1,23 @@
+package com.example.android.kaunbanegacrorepati;
+
+import android.provider.BaseColumns;
+
+public  final class Quizcontract {
+    private  Quizcontract()
+    {    }
+    public static  class Questiontable implements BaseColumns {
+        public static final String TABLE_NAME =" quiz_questions";
+        public static final String Question =" question";
+        public static final String OPTION_1 =" op1";
+        public static final String OPTION_2 =" op2";
+        public static final String OPTION_3 =" op3";
+        public static final String OPTION_4 =" op4";
+        public static final String CORRECT =" correct";
+
+
+
+
+    }
+
+
+}
