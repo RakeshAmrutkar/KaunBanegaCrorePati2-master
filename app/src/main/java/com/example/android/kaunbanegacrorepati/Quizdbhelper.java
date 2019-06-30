b274bf68b486b8f008a6b257472a911925568e83
sahil9420 <sahilpavaskar123@gmail.com>
2019-06-30 18:19:48 +0530

    Data is received from database

--- app/src/main/java/com/example/android/kaunbanegacrorepati/Quizdbhelper.java
+++ app/src/main/java/com/example/android/kaunbanegacrorepati/Quizdbhelper.java
@@ -2,9 +2,14 @@ package com.example.android.kaunbanegacrorepati;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import com.example.android.kaunbanegacrorepati.Quizcontract.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Quizdbhelper<Private> extends SQLiteOpenHelper {
 
     private static final  String  DATABASE_NAME ="KBC.db";
@@ -18,8 +23,8 @@ public class Quizdbhelper<Private> extends SQLiteOpenHelper {
         this.db = db;
         db = this.getWritableDatabase();
         final String Query = " create table "+ Questiontable.TABLE_NAME +"(" + Questiontable._ID +"INTEGER PRIMARY KEY AUTOINCREMENT, "+
-                 Questiontable.Question + "TEXT, " + Questiontable.OPTION_1 +"TEXT," + Questiontable.OPTION_2 +"TEXT,"+
-                  Questiontable.OPTION_3 +"TEXT," + Questiontable.OPTION_4 +"TEXT,"+ Questiontable.CORRECT +"INTEGER," +")";
+                 Questiontable.Question_Col + " TEXT, " + Questiontable.OPTION_1 +" TEXT, " + Questiontable.OPTION_2 +" TEXT, "+
+                  Questiontable.OPTION_3 +" TEXT, " + Questiontable.OPTION_4 +" TEXT, "+ Questiontable.CORRECT +" INTEGER " +")";
        db.execSQL(Query);
     }
     @Override
@@ -32,23 +37,37 @@ public class Quizdbhelper<Private> extends SQLiteOpenHelper {
     private void fillQuestiontable()
     {
       Question question1 = new Question( "WHAT IS SQL ?"  , "  language", "database ","RElational database" , "software", 2  ) ;
-       adddatabasequestions (question1);
+       addDatabaseQuestions (question1);
     }
-    private void adddatabasequestions(Question question)
-
+    private void addDatabaseQuestions(Question question)
     {
         ContentValues contentValues = new ContentValues();
-        contentValues.put(Questiontable.Question , question.getQuestion());
+        contentValues.put(Questiontable.Question_Col , question.getQuestion());
         contentValues.put(Questiontable.OPTION_1 , question.getOption1());
         contentValues.put(Questiontable.OPTION_2 , question.getOption2());
         contentValues.put(Questiontable.OPTION_3 , question.getOption3());
         contentValues.put(Questiontable.OPTION_4 , question.getOption4());
         contentValues.put(Questiontable.CORRECT , question.getCorrect());
         db.insert(Questiontable.TABLE_NAME,null,contentValues);
-
-
-
-
-
     }
+    private List<Question>  getDatabaseQuestions(){
+        List<Question> questions= new ArrayList<>();
+        db=getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + Questiontable.TABLE_NAME, null );
+        if(cursor.moveToFirst()){
+            do{
+                Question question= new Question();
+                question.setQuestion(cursor.getString(cursor.getColumnIndex(Questiontable.Question_Col)));
+                question.setOption1(cursor.getString(cursor.getColumnIndex(Questiontable.OPTION_1)));
+                question.setOption2(cursor.getString(cursor.getColumnIndex(Questiontable.OPTION_2)));
+                question.setOption3(cursor.getString(cursor.getColumnIndex(Questiontable.OPTION_3)));
+                question.setOption4(cursor.getString(cursor.getColumnIndex(Questiontable.OPTION_4)));
+                question.setCorrect(cursor.getInt(cursor.getColumnIndex(Questiontable.CORRECT)));
+                questions.add(question);
+            }while(cursor.moveToNext());
+        }
+        cursor.close();
+        return questions;
+    }
+
 }
